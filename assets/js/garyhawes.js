091cf1f705f1559866ac5dac6ef50de2b0a71cad
Gary Hawes <gary.hawes@gmail.com>
2017-02-08 10:13:50 +0000

    Adding in styles for headers

--- assets/js/garyhawes.js
+++ assets/js/garyhawes.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function() {
   $(document).ready(function () {
-      $('.carousel').carousel({
-          interval: 5000 //@todo changes the speed
-      })
+      //
   });
 });
