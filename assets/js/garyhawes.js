2ad948220674b1e1924475da5462fecefdff4bef
Gary Hawes <gary.hawes@gmail.com>
2017-02-21 15:41:36 +0000

    Updating the styling, removing blog listing.

--- assets/js/garyhawes.js
+++ assets/js/garyhawes.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function() {
   $(document).ready(function () {
-      //
+      $('.grid').isotope({
+          itemSelector: '.grid-item',
+          layoutMode: 'masonryHorizontal',
+          masonryHorizontal: {
+              rowHeight: 50,
+              gutter: 10
+          }
+      });
   });
 });
